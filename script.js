ca12fe7cf7f7d89fd04b866820489e138d44cd13
JorgeVillatoroAvila <143761786+JorgeVillatoroAvila@users.noreply.github.com>
2023-09-05 15:19:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const apiUrl = 'https://api.themoviedb.org/3';
 const genreSelect = document.getElementById('genreSelect');
 const moviesContainer = document.getElementById('moviesContainer');
 const toggleLanguageButton = document.getElementById('toggleLanguage');
+const prevPageButton = document.getElementById('prevPage');
+const nextPageButton = document.getElementById('nextPage');
 let currentLanguage = 'en';
+let currentPage = 1;
 
-// Función para obtener los géneros desde la API  JW
+// Función para obtener los géneros desde la API
 async function fetchGenres() {
   try {
     const response = await fetch(`${apiUrl}/genre/movie/list?api_key=${apiKey}&language=${currentLanguage}`);
@@ -22,13 +25,13 @@ async function fetchGenres() {
   }
 }
 
-// Función para obtener y mostrar las películas JW
-async function fetchMovies(genreId = '') {
+// Función para obtener y mostrar las películas
+async function fetchMovies(genreId = '', page = 1) {
   try {
-    const response = await fetch(`${apiUrl}/movie/popular?api_key=${apiKey}&language=${currentLanguage}&with_genres=${genreId}`);
+    const response = await fetch(`${apiUrl}/movie/popular?api_key=${apiKey}&language=${currentLanguage}&with_genres=${genreId}&page=${page}`);
     const data = await response.json();
     moviesContainer.innerHTML = '';
-    data.results.forEach(movie => {
+    data.results.slice(0, 10).forEach(movie => { // Limita a 10 películas por página
       const movieDiv = document.createElement('div');
       movieDiv.classList.add('movie');
       movieDiv.innerHTML = `
@@ -43,19 +46,33 @@ async function fetchMovies(genreId = '') {
   }
 }
 
+// Actualizar página al hacer clic en "Siguiente"
+nextPageButton.addEventListener('click', () => {
+  currentPage++;
+  fetchMovies(genreSelect.value, currentPage);
+});
+
+// Actualizar página al hacer clic en "Anterior"
+prevPageButton.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchMovies(genreSelect.value, currentPage);
+  }
+});
+
 // Cambiar idioma
 toggleLanguageButton.addEventListener('click', () => {
   currentLanguage = currentLanguage === 'en' ? 'es' : 'en';
-  fetchGenres();
-  fetchMovies();
+  fetchGenres(); // Actualiza los géneros al cambiar el idioma
+  fetchMovies(genreSelect.value, currentPage);
 });
 
-// Cargar géneros y películas al inicio JW1700
+// Cargar géneros y películas al inicio
 fetchGenres();
 fetchMovies();
 
 // Cambiar películas al seleccionar un género
 genreSelect.addEventListener('change', () => {
-  const selectedGenre = genreSelect.value;
-  fetchMovies(selectedGenre);
+  currentPage = 1; // Resetear la página al cambiar el género
+  fetchMovies(genreSelect.value, currentPage);
 });
